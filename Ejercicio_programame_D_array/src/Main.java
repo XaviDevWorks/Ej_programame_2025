73b0b2ff0237fb6d7dadc15e6970265a209c3e94
XavierFarrus <xfarmart@gmail.com>
2025-03-14 11:40:41 +0100

    Add files via upload

---
+++ Ejercicio_programame_D_array/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dime el número de casos de entrada:");
+        int cas = scanner.nextInt();
+        int[] resultados = new int[cas];
+
+        for (int i = 0; i < cas; i++) {
+            System.out.println("Caso de prueba " + (i + 1));
+            System.out.println("Dime los tres valores:");
+
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            int resultadoA = a * 3;
+            int resultadoB = b * 2;
+            int resultadoC = c * 1;
+
+            int resultado = resultadoA + resultadoB + resultadoC;
+
+            resultados[i] = resultado;
+        }
+
+        System.out.println("Resultados:");
+        for (int i = 0; i < cas; i++) {
+            System.out.println(resultados[i]);
+        }
+    }
+}
